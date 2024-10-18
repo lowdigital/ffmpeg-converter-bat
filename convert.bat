4bf648ce4f9a8ba239ec454ac54f391412dd2160
lowdigital <lowdigital@x-mail.ru>
2024-10-18 19:56:20 +0500

    Add files via upload

--- convert.bat
+++ convert.bat
@@ -3,10 +3,10 @@ chcp 65001 >nul
 setlocal enabledelayedexpansion
 
 :: trim the beginning
-set "trim_time_start=0.01"
+set "trim_time_start=0.00"
 
 :: trim the end
-set "trim_time_end=0.01"
+set "trim_time_end=0.00"
 
 if "%~1"=="" (
     echo Usage: Drag and drop a video file onto this batch file or provide the path to the file as an argument.
@@ -24,13 +24,34 @@ set "output_file=%input_dir%%file_name%_fixed%file_extension%"
 set /p original_bitrate=<temp_bitrate.txt
 del temp_bitrate.txt
 if "%original_bitrate%"=="" (
-    echo Error: Failed to extract bitrate of source video.
+    echo Ошибка: не удалось извлечь битрейт исходного видео.
     pause
     exit /b 1
 )
-echo Original bitrate: %original_bitrate%
-echo Trimming video from %trim_time_start% to %trim_time_end% seconds.
-"%ffmpeg_path%" -i "%input_file%" -ss %trim_time_start% -to %trim_time_end% -c:v h264_nvenc -b:v %original_bitrate% -g 30 -c:a aac -b:a 128k -movflags +faststart "%output_file%"
+echo Оригинальный битрейт: %original_bitrate%
+"%ffprobe_path%" -v quiet -select_streams v:0 -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "%input_file%" > temp_duration.txt
+set /p total_duration=<temp_duration.txt
+del temp_duration.txt
+if "%total_duration%"=="" (
+    echo Error: Failed to extract duration of the video.
+    pause
+    exit /b 1
+)
+set "final_duration=%total_duration: =%"
+set /a "final_duration=%total_duration% - %trim_time_end%"
+if %final_duration% LEQ 0 (
+    echo Error: The trimmed end time is larger than the video duration.
+    pause
+    exit /b 1
+)
+echo Trimming video from %trim_time_start% to %final_duration% seconds.
+"%ffmpeg_path%" -i "%input_file%" -ss %trim_time_start% -t %final_duration% -c:v h264_nvenc -b:v %original_bitrate% -g 30 -c:a aac -b:a 128k -movflags +faststart "%output_file%"
+
+if errorlevel 1 (
+    echo Error processing video with FFmpeg.
+    pause
+    exit /b 1
+)
 del "%input_file%"
 echo Done! The video is processed and saved as "%output_file%".
 pause
